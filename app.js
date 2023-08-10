7216de6fcd28892f765b3d3aea2af744d93d44c8
apple <apple@apples-MacBook-Pro.local>
2023-08-10 23:30:01 +0530

    use lodash

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
 const http = require('http')
-
-const server = http.createServer((req, res) => {
-   // console.log(req);
-    if (req.url === "/") {
-       // res.write("Helo we are alomost there")
-        res.end("Helo we are alomost there")
-
-    } else if (req.url === "/about") {
-      //  res.write("It's about page")
-        res.end("It's about page")
-    }
-
-    res.end(`
-<h1>Oops</h1>
-<p>Something went wrong</p>
-`)
-})
-
-server.listen(5001)
+const _ = require('lodash')
+const items = [1, [1234], 1,[23,3],[23],[3]];
+const newItems = _.flattenDepth(items)
+console.log(newItems);
